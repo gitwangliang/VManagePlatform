2351dae0bab5c72c6051a1f2da4dac54b7697835
wangliang <894649670@qq.com>
2018-12-13 15:53:39 +0800

    test

--- ssh.py
+++ ssh.py
@@ -4,5 +4,5 @@ import paramiko
 ssh = paramiko.SSHClient()
 ssh.load_system_host_keys() ####获取ssh key密匙，默认在~/.ssh/knows_hosts
 ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-ssh.connect(hostname = "10.10.10.27",port="22222",username="root")
+ssh.connect(hostname = "10.10.10.27",port="22222",username="root",allow_agent=False,look_for_keys=False)
 print(ssh)
